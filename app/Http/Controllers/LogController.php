4177992a7a7756f56a01af272acef41ae325bbae
Morgan Campbell <jmorgancampbell@gmail.com>
2015-09-22 00:32:13 -0500

    Handle generic things

--- app/Http/Controllers/LogController.php
+++ app/Http/Controllers/LogController.php
@@ -11,11 +11,11 @@ class LogController extends Controller
     public function save()
     {
         $filters = app('request')->input();
-        $res = DB::insert('INSERT INTO logs(user_id, baby_id, thing_id, data, notes) VALUES(:user_id, :baby_id, :thing_id, :data_milk, :notes)', [
+        $res = DB::insert('INSERT INTO logs(user_id, baby_id, thing_id, data, notes) VALUES(:user_id, :baby_id, :thing_id, :data, :notes)', [
             'user_id' => (int) $filters['user_id'],
             'baby_id' => (int) $filters['baby_id'],
             'thing_id' => $filters['thing_id'],
-            'data_milk' => $filters['data_milk'],
+            'data' => $filters['data'],
             'notes' => $filters['notes']
         ]);
         dd($res);
