eb2b8a8c44d0919377146753eb0f92ecae1f5481
Morgan Campbell <jmorgancampbell@gmail.com>
2015-11-07 17:46:34 -0600

    Log filters

--- app/Http/Controllers/LogController.php
+++ app/Http/Controllers/LogController.php
@@ -11,13 +11,25 @@ class LogController extends Controller
     public function index()
     {
         $filters = app('request')->input();
-        $logs = DB::select('SELECT * FROM logs');
+
+        $query = 'SELECT * FROM logs WHERE baby_id = :baby_id AND created_at >= DATE_SUB(NOW(), INTERVAL 12 HOUR) ORDER BY created_at DESC';
+        // if (array_key_exists('date', $filters) {
+        //     $query = 'SELECT * FROM logs WHERE ';
+        // }
+
+        $logs = DB::select($query, [
+            'baby_id' => (int) $filters['baby_id']
+        ]);
+        // $babies = DB::select('SELECT * FROM babies WHERE active = :active AND id = :id', [
+        //     'active' => (boolean) $filters['active'],
+        //     'id' => (int) $filters['id']
+        // ]);
 
         $logs = array_map(function($log)
         {
             return [
                 'user_id' => (int) $log->user_id,
-                'baby_id' =>(int) $log->baby_id,
+                'baby_id' => (int) $log->baby_id,
                 'thing_id' => $log->thing_id,
                 'data' => $log->data,
                 'notes' => $log->notes,
